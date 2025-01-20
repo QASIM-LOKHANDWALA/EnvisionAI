95e77448322cf6069ac8d784227e0f93193cebdc
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-01-20 17:07:07 +0530

    Implement JWT And Create Basic Signin Route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,38 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import validator from "validator";
+import jwt from "jsonwebtoken";
 import UserSchema from "../mongodb/models/user.js";
 
 const router = express.Router();
 
-router.post("/login", async (req, res) => {});
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      throw Error("All fields must be filled.");
+    }
+
+    const user = await UserSchema.findOne({ email });
+    if (!user) {
+      throw Error("Incorrect Email.");
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      throw Error("Incorrect Password.");
+    }
+
+    const token = createToken(user._id);
+    res.status(200).json({ email, token, success: true });
+  } catch (error) {
+    res.status(400).json({ error: error.message, success: false });
+  }
+});
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
@@ -28,7 +55,8 @@ router.post("/signup", async (req, res) => {
     const hash = await bcrypt.hash(password, salt);
 
     const user = await UserSchema.create({ email, password: hash });
-    res.status(200).json({ email, user, success: true });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token, success: true });
   } catch (error) {
     res.status(400).json({ error: error.message, success: false });
   }
